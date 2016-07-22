30b089955cf3343bc5a1e51ece88565586e4714b
Carlos Cornejo <carlos.cornejo@williamhill.com>
2016-07-22 16:34:23 +0100

    module1-lesson3

--- m1-lesson3/src/main/java/com/baeldung/lss/spring/LssSecurityConfig.java
+++ m1-lesson3/src/main/java/com/baeldung/lss/spring/LssSecurityConfig.java
@@ -2,6 +2,7 @@ package com.baeldung.lss.spring;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 
@@ -20,6 +21,18 @@ public class LssSecurityConfig extends WebSecurityConfigurerAdapter {
             inMemoryAuthentication().
             withUser("user").password("pass").
             roles("USER");
+    } // @formatter:on
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception { // @formatter:off
+        http
+            .authorizeRequests()
+                .antMatchers("/delete/**").hasRole("ADMIN")
+                .anyRequest().authenticated()
+                .and()
+                    .formLogin()
+                .and()
+                    .httpBasic();
     } // @formatter:on
 
 }
