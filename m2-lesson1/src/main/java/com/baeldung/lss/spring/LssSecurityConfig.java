afdc6496fd3b5f7eb307083996a81ec7eadfce31
Carlos Cornejo <carlos.cornejo@williamhill.com>
2016-07-26 13:52:53 +0100

    added lesson 1

--- m2-lesson1/src/main/java/com/baeldung/lss/spring/LssSecurityConfig.java
+++ m2-lesson1/src/main/java/com/baeldung/lss/spring/LssSecurityConfig.java
@@ -9,12 +9,6 @@ import org.springframework.security.config.annotation.web.configuration.WebSecur
 @EnableWebSecurity
 public class LssSecurityConfig extends WebSecurityConfigurerAdapter {
 
-    public LssSecurityConfig() {
-        super();
-    }
-
-    //
-
     @Autowired
     public void configureGlobal(AuthenticationManagerBuilder auth) throws Exception { // @formatter:off 
         auth.
@@ -27,19 +21,17 @@ public class LssSecurityConfig extends WebSecurityConfigurerAdapter {
     protected void configure(HttpSecurity http) throws Exception { // @formatter:off
         http
         .authorizeRequests()
+                .antMatchers("/signup", "/user/register").permitAll()
                 .anyRequest().authenticated()
-        
         .and()
-        .formLogin().
-            loginPage("/login").permitAll().
-            loginProcessingUrl("/doLogin")
+            .formLogin().
+                loginPage("/login").permitAll().
+                loginProcessingUrl("/doLogin")
 
         .and()
-        .logout().permitAll().logoutUrl("/logout")
-        
+            .logout().permitAll().logoutUrl("/logout")
         .and()
-        .csrf().disable()
-        ;
-    }
+            .csrf().disable();
+    } // @formatter:on
 
 }
